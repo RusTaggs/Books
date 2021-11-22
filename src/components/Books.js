f9cbdefd340be3d005cc2e2862a434207423af5f
RusTaggs <udachin_2000@mail.ru>
2021-11-22 15:31:13 +0300

    test

--- src/components/Books.js
+++ src/components/Books.js
@@ -24,6 +24,7 @@ export class Books extends Component {
                 .then((data) => {
                     console.log(data);
                 });
+                
     }
 
     handleSearch = (e) => {
